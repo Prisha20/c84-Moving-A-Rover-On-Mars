f888d4f8fc2f4a0d6765ab7aec9809c1e1689ecc
Prisha20 <82700376+Prisha20@users.noreply.github.com>
2021-04-22 13:10:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+canvas = document.getElementById('canvas10');
+ctx = canvas.getContext("2d");
+
+var rover_width = 100;
+var rover_height = 100;
+var rover_x = 15;
+var rover_y = 15;
+var background_image = "mars.jpg";
+var rover_image = "rover.png";
+
+function add()
+{
+    background_imgTag = new Image();
+    background_imgTag.onload = uploadBackground;
+    background_imgTag.src = background_image;
+
+    rover_imgTag = new Image();
+    rover_imgTag.onload = uploadrover;
+    rover_imgTag.src = rover_image;
+}
+
+function uploadBackground() 
+{
+    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadrover()
+{
+    ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+}
+
+window.addEventListener("keydown", keyclick);
+
+function keyclick(e)
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+
+    if (keyPressed == '38')
+    {
+        up();
+        console.log("up");
+    }
+
+    if (keyPressed == '40')
+    {
+        down();
+        console.log("down");
+    }
+
+    if (keyPressed == '37')
+    {
+       left();
+        console.log("left");
+    }
+
+    if (keyPressed == '38')
+    {
+        right();
+        console.log("right");
+    }
+}
